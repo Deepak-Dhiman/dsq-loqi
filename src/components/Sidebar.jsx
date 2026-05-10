b5d30f9be326ca3c492d0fbccddc7bce3945ef03
Deepak Dhiman <99280200+Deepak-Dhiman@users.noreply.github.com>
2026-05-10 15:32:27 +0530

    Style sidebar elements and group them

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,38 +1,51 @@
 export default function Sidebar(props) {
   return (
     <section className="sidebar">
-      <input type="color" value={props.bgColor} onChange={props.addBg} />
+      <div className="bg-color">
+        <label htmlFor="background-color">Background Colour</label>
+        <input
+          type="color"
+          value={props.bgColor}
+          onChange={props.addBg}
+          id="background-color"
+        />
+      </div>
+      <div className="text-styles">
+        <button onClick={props.addText}>Add Text</button>
 
-      <button onClick={props.addText}>Add Text</button>
+        {/* Color Picker for Text */}
+        <label>Text Color:</label>
+        <input
+          type="color"
+          onChange={(e) => props.changeTextProperty("fill", e.target.value)}
+        />
 
-      {/* Color Picker for Text */}
-      <label>Text Color:</label>
-      <input
-        type="color"
-        onChange={(e) => props.changeTextProperty("fill", e.target.value)}
-      />
+        {/* Font Size Slider */}
+        <label>Size:</label>
+        <input
+          type="range"
+          min="10"
+          max="100"
+          onChange={(e) =>
+            props.changeTextProperty("fontSize", parseInt(e.target.value))
+          }
+        />
 
-      {/* Font Size Slider */}
-      <label>Size:</label>
-      <input
-        type="range"
-        min="10"
-        max="100"
-        onChange={(e) =>
-          props.changeTextProperty("fontSize", parseInt(e.target.value))
-        }
-      />
-
-      {/* Font Selector */}
-      <select
-        onChange={(e) => props.changeTextProperty("fontFamily", e.target.value)}
-      >
-        <option value="helvetica">Helvetica</option>
-        <option value="courier">Courier</option>
-        <option value="times-new-roman">Times New Roman</option>
-      </select>
-
-      <input type="file" accept="image/*" onChange={props.addImg} />
+        {/* Font Selector */}
+        <select
+          onChange={(e) =>
+            props.changeTextProperty("fontFamily", e.target.value)
+          }
+        >
+          <option value="helvetica">Helvetica</option>
+          <option value="courier">Courier</option>
+          <option value="times-new-roman">Times New Roman</option>
+        </select>
+      </div>
+      <div className="image-styles">
+        <label htmlFor="file-upload" className="file-upload">Upload File</label>
+        <input type="file" accept="image/*" onChange={props.addImg} id="file-upload"/>
+      </div>
     </section>
   );
 }
